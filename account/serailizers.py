997d6214bd7a0b34f30ae39bc0e32a0b51f91b0f
Z3TEL <elabdukadyrov@gmail.com>
2021-08-13 20:03:20 +0600

    Finished auth

--- account/serailizers.py
+++ account/serailizers.py
@@ -1,4 +1,6 @@
 from django.contrib.auth import get_user_model, authenticate
+from django.core.mail import send_mail
+from django.utils.crypto import get_random_string
 from rest_framework import serializers
 
 
@@ -63,11 +65,70 @@ class ActivationSerializer(serializers.Serializer):
 
 
 class ChangePasswordSerializer(serializers.Serializer):
-    pass
+    old_password = serializers.CharField(max_length=6, required=True)
+    new_password = serializers.CharField(max_length=6, required=True)
+    new_password_confirm = serializers.CharField(max_length=6, required=True)
 
+    def validate_old_password(self, old_password):
+        request = self.context.get('request')
+        user = request.user
+        if not request.user.check_password(old_password):
+            raise serializers.ValidationError('Ввидете верный пороль')
+        return  old_password
+
+    def validate(self, attrs):
+        new_pass1 = attrs.get('new_password')
+        new_pass2 = attrs.get('new_password_confirm')
+        if new_pass1 != new_pass2:
+            raise serializers.ValidationError('Пароли не совпадают')
+
+    def set_new_password(self):
+        new_pass = self.validated_data.get('new_password')
+        user = self.context.get('request').user
+        user.set_password(new_pass)
+        user.save()
 
 class ForgotPasswordSerializer(serializers.Serializer):
-    pass
+    email = serializers.EmailField(required=True)
+
+    def validator_email(self, email):
+        if not User.objects.filter(email=email).exists():
+            raise serializers.ValidationError('Такой пользователь не существует')
+        return email
+
+    def send_verification_email(self):
+        email = self.validated_data.get('email')
+        user = User.objects.get(email=email)
+        user.create_activation_code()
+        send_mail('Восстановление пароля',
+                  f'Ваш код восстановление : {user.activation_code}',
+                  'test1@gmail.ru',
+                  {user.email})
+
+
+class ForgotPasswordCompleteSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    code = serializers.CharField(required=True)
+    password = serializers.CharField(max_length=6, required=True)
+    password_confirm = serializers.CharField(max_length=6, required=True)
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        code = attrs.get('code')
+        password1 = attrs.get('password')
+        password2 = attrs.get('password_confirm')
+        if not User.objects.filter(email=email, activation_code=code).exists():
+            raise serializers.ValidationError('Пользователь не найден')
+        if password1 != password2:
+            raise serializers.ValidationError('Пороли не совпадают')
+        return attrs
+
+    def set_new_password(self):
+        email = self.validated_data.get('email')
+        password = self.validated_data.get('password')
+        user = User.objects.get(email=email)
+        user.set_password(password)
+        user.save()
 
 
 class LoginSerializer(serializers.Serializer):
@@ -93,3 +154,24 @@ class LoginSerializer(serializers.Serializer):
             raise serializers.ValidationError("Email и пороль обезательные")
         data['user'] = user
         return data
+
+
+#     Второй вариант ForgotPasswordSerializer
+#
+# class ForgotPasswordSerializer(serializers.Serializer):
+#     email = serializers.EmailField(required=True)
+#
+#     def validator_email(self, email):
+#         if not User.objects.filter(email=email).exists():
+#             raise serializers.ValidationError('Такой пользователь не существует')
+#         return email
+#
+#     def send_verification_mail(self):
+#         email = self.validated_data.get('email')
+#         user = User.objects.get(email=email)
+#         random_password = get_random_string(length=10)
+#         user.set_password(random_password)
+#         send_mail('Восстановление пароля',
+#                   f'Ваш новый пароль : {random_password}',
+#                   'test1@gmail.ru',
+#                   {user.email})
--- account/views.py
+++ account/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
+from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 
 from .serailizers import (RegistrationSerializer, ActivationSerializer, ChangePasswordSerializer,
-                          ForgotPasswordSerializer, LoginSerializer)
+                          ForgotPasswordSerializer, LoginSerializer, ForgotPasswordCompleteSerializer)
 
 
 
@@ -35,16 +37,36 @@ class LoginView(ObtainAuthToken):
     serializer_class = LoginSerializer
 
 
-class LogoutView():
-    pass
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        user = request.user
+        Token.objects.filter(user=user).delete()
+        return Response('Вы усрешно разлогинились')
 
 
 
-class ChangePasswordView():
-    pass
+class ChangePasswordView(APIView):
+    def post(self, request):
+        serializer = ChangePasswordSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.set_new_password()
+            return Response('Пороль успешно обнавлён')
+
+
+class ForgotPasswordView(APIView):
+    def post(self, request):
+        serializer = ForgotPasswordSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.send_verification_email()
+            return Response('Вас выслано сообщение на восстановления')
 
 
 
-
-class ForgotPasswordView():
-    pass
+class ForgotPasswordCompleteView(APIView):
+    def post(self, request):
+        serializer = ForgotPasswordCompleteSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.send_verification_email()
+            return Response('Пороль успешно обнавлён')
